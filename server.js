5926317bc26c1dd2f0941a3478bad9c142993271
Harshit Jain <harshitjain1371999@gmail.com>
2017-09-05 21:53:39 +0530

    corrected routes and events page added

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ require('dotenv').load();
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+app.use('/', express.static(process.cwd() + '/public'))
+app.use('.', express.static(process.cwd()));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 app.use(session({
 	secret: 'secretClementine',
