832e1459da06ed242bc0248becb584b6cea444c8
Arpan-sopho <mangalarpan@gmail.com>
2017-09-06 12:31:57 +0530

    Added node mailer

---
+++ public/js/mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'feedback.acesacmiitd@gmail.com',
+        pass: 'Ui98n$sN!' // currently hard-coded and publicly visible, to be removed later
+    }
+});
+
+var mailOptions = {
+    from: 'feedback.acesacmiitd@gmail.com',
+    to: 'feedback.acesacmiitd@gmail.com',
+    subject: 'MESSAGE FROM WEBSITE',
+    text: 'sending my feedback'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
