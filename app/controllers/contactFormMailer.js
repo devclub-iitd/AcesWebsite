f38a710b9630ca329f68c615db23291b151e4f03
Arpan-sopho <mangalarpan@gmail.com>
2017-09-08 22:46:08 +0530

    Mail working with iitd webmail

--- app/controllers/contactFormMailer.js
+++ app/controllers/contactFormMailer.js
@@ -5,20 +5,20 @@ var nodemailer = require('nodemailer');
 module.exports = {
     // Code for nodeMailer
     transporter: nodemailer.createTransport({
-        host: 'smtp.gmail.com',
+        host: 'smtp.iitd.ernet.in',
        // service: 'gmail',
         port: 465,
         secure: true,
         auth: {
-            user: 'feedback.acesacmiitd@gmail.com',
-            pass: 'Ui98n$sN!' // The password is at server side and is not visible to the client
+            user: 'kerbros_id',
+            pass: 'kerbros_pass' // The password is at server side and is not visible to the client
         },
-        proxy: 'http://proxy22.iitd.ac.in:3128',
-        https_proxy: 'https://proxy22.iitd.ac.in:3128'
+       // proxy: 'http://proxy22.iitd.ac.in:3128',
+       // https_proxy: 'https://proxy22.iitd.ac.in:3128'
     }),
 
     mailOptions: {
-        from: 'feedback.acesacmiitd@gmail.com',
+        from: 'kerbros_id@iitd.ac.in',
         to: 'feedback.acesacmiitd@gmail.com',
         subject: 'MESSAGE FROM WEBSITE',
         html: 'Empty Feedback'
