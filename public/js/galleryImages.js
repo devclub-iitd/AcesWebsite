eae356e812040af1443ee1b25c8c856d055f038d
Rajas Bansal <rajas@Rajas-MacBook-Pro.local>
2017-09-09 19:43:24 +0530

    Dynamically loading image folders

--- public/js/galleryImages.js
+++ public/js/galleryImages.js
@@ -11,6 +11,7 @@
             if (image.split('.')[0] == 'main') main = image;
         });
         main = main || images[0];
+        //console.log(eventName);
         li.innerHTML = '<h3>' + eventName + '</h3><a class="open_fancybox" href=' + '\"./img/events/' + eventName + '/' + main + '\"><img class="top img-responsive" src=' + '\"./img/events/' + eventName + '/' + main + '\" width="350" height="300" /></a>';
         gallery.append(li);
 
@@ -31,19 +32,23 @@
         return data;
     }).then(function(data) {
         var images = {};
+        //console.log(data);
         data.forEach(function(event, index) {
                 images[index+1] = [];
                 event.slice(1).forEach(function(image, pos) {
-                    images[index+1].push({
-                        'href': './img/events/event' + (index+1) + '/' + image,
-                        'title': 'Gallery ' + (index+1) + ' - ' + (pos + 1)
-                    });
+                    if (image!=".DS_Store"){
+                        images[index+1].push({
+                            'href': './img/events/'+ event[0]+ '/' + image,
+                            'title': 'Gallery '+ event[0] + ' - ' + (pos + 1)
+                        });
+                    }
                 });
         });
         return images
     }).then(function(images) {
 
         $(".open_fancybox").click(function() {
+            //console.log(images[$(this).parent().index() + 1]);
             $.fancybox.open(images[$(this).parent().index() + 1], {
                 padding: 0
             });
