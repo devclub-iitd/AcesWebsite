9eb05778028fd4ef18b1029a8d0115017cb0eef3
Rajas Bansal <rajas@Rajas-MacBook-Pro.local>
2017-09-09 18:43:27 +0530

    Added directory checking to gallerycontroller

--- app/controllers/galleryController.js
+++ app/controllers/galleryController.js
@@ -7,12 +7,14 @@ module.exports = {
     readDir: function() {
         var data = [];
         fs.readdirSync(path).forEach(folder => {
+            if (fs.statSync(path + '/' + folder).isDirectory()){
             var dir = [];
             dir.push(folder);
             fs.readdirSync(path + '/' + folder).forEach(file=> {
                 dir.push(file);
             });
             data.push(dir);
+        }
         });
         return data;
     }
