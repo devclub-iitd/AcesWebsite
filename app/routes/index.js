b5aefdbf3d1fe4f489cefcf43b50ae5e1a707ea9
Siddhant Bhakar <sidbhakar@gmail.com>
2017-12-06 19:17:51 +0000

    created superuser and added photo upload feature

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,8 @@
 var path = process.cwd();
 var contactFormMailer = require('../controllers/contactFormMailer.js');
 var galleryController = require('../controllers/galleryController.js');
-module.exports = function(app, passport) {
+var unzip = require('unzip');
+module.exports = function(app, fs) {
 
 	function isLoggedIn(req, res, next) {
 		if (req.isAuthenticated()) {
@@ -14,6 +15,14 @@ module.exports = function(app, passport) {
 		}
 	}
 
+	// Authentication and Authorization Middleware
+	var auth = function(req, res, next) {
+		if (req.session && req.session.user === "admin" && req.session.admin)
+			return next();
+		else
+			return res.redirect('/login');
+	};
+
 	app.route('/')
 		.get(function(req, res) {
 			res.sendFile(path + '/public/index.html');
@@ -22,9 +31,9 @@ module.exports = function(app, passport) {
 			//console.log(req.body);
 			contactFormMailer.mailOptions["subject"] = 'MESSAGE FROM WEBSITE: ' + contactFormMailer.escapeHtml(req.body["subject"]);
 			contactFormMailer.mailOptions["html"] = "<br /><p>" + contactFormMailer.escapeHtml(req.body["message"]) + "</p><br /><span>From :</span><br /><span>  " +
-			contactFormMailer.escapeHtml(req.body["name"]) + "</span><br /><span>  " + contactFormMailer.escapeHtml(req.body["email"]) + "</span>";
+				contactFormMailer.escapeHtml(req.body["name"]) + "</span><br /><span>  " + contactFormMailer.escapeHtml(req.body["email"]) + "</span>";
 			res.sendStatus(200);
-			
+
 			//contactFormMailer.sendFeedback();
 			//res.redirect('/public/index.html');
 		});
@@ -39,5 +48,42 @@ module.exports = function(app, passport) {
 	app.route('/images')
 		.get(function(req, res) {
 			res.send(galleryController.readDir());
+		});
+	app.route('/login')
+		.get(function(req, res) {
+			res.sendFile(path + '/public/login.html');
+		})
+		.post(function(req, res) {
+			if (!req.body.username || !req.body.password) {
+				res.send('login failed');
+			}
+			else if (req.body.username === "admin" || req.body.password === "admin") {
+				req.session.user = "admin";
+				req.session.admin = true;
+				res.redirect('/admin');
+			}
+			else {
+				res.send('login failed');
+			}
+		});
+	app.route('/admin')
+		.get(auth, function(req, res) {
+			res.sendFile(path + '/public/admin.html');
+		});
+	app.route('/logout')
+		.get(function(req, res) {
+			req.session.destroy();
+			res.send("logout success!");
+		});
+	app.route('/pics')
+		.post(function(req, res) {
+			if (!req.files)
+				return res.status(400).send('No files were uploaded.');
+			req.files.file.mv(path + '/public/img/events/uploaded', function(err) {
+				if (err)
+					return res.status(500).send(err);
+				fs.createReadStream(path + '/public/img/events/uploaded').pipe(unzip.Extract({ path: path + '/public/img/events/'}));
+				res.send('File uploaded!');
+			});
 		});
 };
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 var nodemailer = require('nodemailer');
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
 
 var app = express();
 require('dotenv').load();
@@ -20,10 +22,11 @@ app.use('.', express.static(process.cwd()));
 // app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 app.use(session({
-	secret: 'secretClementine',
-	resave: false,
+	secret: '2C44-4D44-WppQ38S',
+	resave: false, // change to true
 	saveUninitialized: true
 }));
+app.use(fileUpload({ safeFileNames: true, preserveExtension: true }));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
@@ -34,9 +37,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-routes(app, passport);
+routes(app, fs);
 
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
+app.listen(port, function() {
 	console.log('Node.js listening on port ' + port + '...');
 });
