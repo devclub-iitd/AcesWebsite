397dc4cce30ff1ac3ff8c455b627b23cab5464b8
Aman Agrawal <aman71197@gmail.com>
2018-04-17 23:48:29 +0530

    changed a little ui in admin page

--- app/controllers/onLoadControllerUsers.js
+++ app/controllers/onLoadControllerUsers.js
@@ -17,29 +17,29 @@
             data.sort(function(a, b) {
                 return a.order - b.order;
             }).forEach(function(user) {
-                console.log(user.order);
+                // console.log(user.order);
                 var tr = document.createElement('tr');
                 tr.setAttribute('class', 'bill-item');
                 tr.setAttribute('id', user._id);
-                tr.innerHTML = "<td><button class='btn-primary'><i class='fa fa-arrow-up up' aria-hidden='true'></i></button>&nbsp;<button class='btn-primary '><i class='fa fa-arrow-down down' aria-hidden='true'></i></button></td><td>" + user.name + "</td><td>" + user.username + "</td><td>" + user.admin + "</td><td><button class='btn2 userdel' >delete user</button></td>";
+                tr.innerHTML = "<td>" + user.name + "</td><td>" + user.username + "</td><td>" + user.admin + "</td><td><button class='btn-primary'><i class='fa fa-arrow-up up' aria-hidden='true'></i></button>&nbsp;<button class='btn-primary '><i class='fa fa-arrow-down down' aria-hidden='true'></i></button></td>"+"<td><button class='btn2 userdel' >delete user</button></td>";
                 userList.append(tr);
             });
         }).then(function() {
             document.querySelectorAll('.userdel').forEach(btn => btn.addEventListener('click', function(e) {
-                console.log(e.target.parentElement.parentElement.id);
+                // console.log(e.target.parentElement.parentElement.id);
                 ajaxFunctions.ajaxRequest('GET', appUrl + '/user_del?' + 'id=' + e.target.parentElement.parentElement.id, function(msg) {
                     window.location.reload(true);
                 });
             }));
             document.querySelectorAll('.up').forEach(btn => btn.addEventListener('click', function(e) {
-                console.log(e.target.parentElement.parentElement.parentElement.id);
+                // console.log(e.target.parentElement.parentElement.parentElement.id);
                 ajaxFunctions.ajaxRequest('GET', appUrl + '/moveup?' + 'id=' + e.target.parentElement.parentElement.parentElement.id, function(msg) {
                     window.location.reload(true);
                 });
             }));
 
             document.querySelectorAll('.down').forEach(btn => btn.addEventListener('click', function(e) {
-                console.log(e.target.parentElement.parentElement.parentElement.id);
+                // console.log(e.target.parentElement.parentElement.parentElement.id);
                 ajaxFunctions.ajaxRequest('GET', appUrl + '/movedown?' + 'id=' + e.target.parentElement.parentElement.parentElement.id, function(msg) {
                     window.location.reload(true);
                 });
